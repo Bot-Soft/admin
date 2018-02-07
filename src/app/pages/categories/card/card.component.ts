aa5644cc418ca23447731a392a5618f828723ecd
Angel Tsvetkov <mrproper86@mail.com>
2018-02-08 00:38:38 +0200

    update

--- src/app/pages/categories/card/card.component.ts
+++ src/app/pages/categories/card/card.component.ts
@@ -1,183 +1,191 @@
-import { AfterViewInit, Component, Input, OnDestroy } from '@angular/core';
-import { NbThemeService } from '@nebular/theme';
+import { AfterViewInit, Component, Input, OnDestroy } from "@angular/core";
+import { NbThemeService } from "@nebular/theme";
 
 declare const echarts: any;
 
 @Component({
-  selector: 'ngx-categories-card',
-  styleUrls: ['./card.component.scss'],
+  selector: "ngx-categories-card",
+  styleUrls: ["./card.component.scss"],
   template: `
-    <nb-card size="xsmall" class="solar-card">
-      <nb-card-header>
-      <a href="/#/bot/items">
+  <nb-card size="small">
+  <nb-card-body>
+    <div class="picture" style.background-image="url({{image}})"></div>
+
+    <div class="details">
+      <div class="title">
         {{title}}
-        </a>
-      </nb-card-header>
-      <nb-card-body>
-        <div echarts [options]="option" class="echart">
-        </div>
-        <div class="info">
-          <div class="value">{{segment}}$</div>
-          <div class="details"><span>out of</span> {{total}}$</div>
-        </div>
-      </nb-card-body>
-    </nb-card>
-  `,
+      </div>
+      <div class="description">
+        {{subtitle}}
+      </div>
+    </div>
+  </nb-card-body>
+  <nb-card-footer>
+  <nb-actions size="medium" fullWidth>
+    <nb-action>
+      <a href="#">Edit</a>
+    </nb-action>
+    <nb-action>
+    <a href="#"><b>Items</b></a>
+  </nb-action>
+    <nb-action>
+    <a href="#">Delete</a>
+  </nb-action>
+  </nb-actions>
+</nb-card-footer>
+</nb-card>
+  `
 })
 export class CategoriesCardComponent implements AfterViewInit, OnDestroy {
-
   @Input() title: string;
-  @Input() total: number;
-  @Input() segment: number;
+  @Input() subtitle: string;
+  @Input() image: string;
 
   private value;
 
   option: any = {};
   themeSubscription: any;
 
-  constructor(private theme: NbThemeService) {
-  }
+  constructor(private theme: NbThemeService) {}
 
   ngAfterViewInit() {
-    this.value = Math.floor((this.segment / this.total) * 100);
-    this.themeSubscription = this.theme.getJsTheme().delay(1).subscribe(config => {
-
-      const solarTheme: any = config.variables.solar;
-
-      this.option = Object.assign({}, {
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)',
-        },
-        series: [
-          {
-            name: ' ',
-            clockWise: true,
-            hoverAnimation: false,
-            type: 'pie',
-            center: ['45%', '50%'],
-            radius: solarTheme.radius,
-            data: [
-              {
-                value: this.value,
-                name: ' ',
-                label: {
-                  normal: {
-                    position: 'center',
-                    formatter: '{d}%',
-                    textStyle: {
-                      fontSize: '22',
-                      fontFamily: config.variables.fontSecondary,
-                      fontWeight: '600',
-                      color: config.variables.fgHeading,
-                    },
-                  },
-                },
-                tooltip: {
-                  show: false,
-                },
-                itemStyle: {
-                  normal: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                      {
-                        offset: 0,
-                        color: solarTheme.gradientLeft,
-                      },
-                      {
-                        offset: 1,
-                        color: solarTheme.gradientRight,
-                      },
-                    ]),
-                    shadowColor: solarTheme.shadowColor,
-                    shadowBlur: 0,
-                    shadowOffsetX: 0,
-                    shadowOffsetY: 3,
-                  },
-                },
-                hoverAnimation: false,
-              },
-              {
-                value: 100 - this.value,
-                name: ' ',
-                tooltip: {
-                  show: false,
-                },
-                label: {
-                  normal: {
-                    position: 'inner',
-                  },
-                },
-                itemStyle: {
-                  normal: {
-                    color: config.variables.layoutBg,
-                  },
-                },
-              },
-            ],
-          },
-          {
-            name: ' ',
-            clockWise: true,
-            hoverAnimation: false,
-            type: 'pie',
-            center: ['45%', '50%'],
-            radius: solarTheme.radius,
-            data: [
-              {
-                value: this.value,
-                name: ' ',
-                label: {
-                  normal: {
-                    position: 'inner',
-                    show: false,
-                  },
-                },
-                tooltip: {
-                  show: false,
-                },
-                itemStyle: {
-                  normal: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                      {
-                        offset: 0,
-                        color: solarTheme.gradientLeft,
-                      },
-                      {
-                        offset: 1,
-                        color: solarTheme.gradientRight,
-                      },
-                    ]),
-                    shadowColor: solarTheme.shadowColor,
-                    shadowBlur: 7,
-                  },
-                },
-                hoverAnimation: false,
-              },
-              {
-                value: 100,
-                name: ' ',
-                tooltip: {
-                  show: false,
-                },
-                label: {
-                  normal: {
-                    position: 'inner',
-                  },
-                },
-                itemStyle: {
-                  normal: {
-                    color: 'none',
-                  },
-                },
-              },
-            ],
-          },
-        ],
-      });
-    });
+    // this.value = Math.floor((this.segment / this.total) * 100);
+    // this.themeSubscription = this.theme.getJsTheme().delay(1).subscribe(config => {
+    //   const solarTheme: any = config.variables.solar;
+    //   this.option = Object.assign({}, {
+    //     tooltip: {
+    //       trigger: 'item',
+    //       formatter: '{a} <br/>{b} : {c} ({d}%)',
+    //     },
+    //     series: [
+    //       {
+    //         name: ' ',
+    //         clockWise: true,
+    //         hoverAnimation: false,
+    //         type: 'pie',
+    //         center: ['45%', '50%'],
+    //         radius: solarTheme.radius,
+    //         data: [
+    //           {
+    //             value: this.value,
+    //             name: ' ',
+    //             label: {
+    //               normal: {
+    //                 position: 'center',
+    //                 formatter: '{d}%',
+    //                 textStyle: {
+    //                   fontSize: '22',
+    //                   fontFamily: config.variables.fontSecondary,
+    //                   fontWeight: '600',
+    //                   color: config.variables.fgHeading,
+    //                 },
+    //               },
+    //             },
+    //             tooltip: {
+    //               show: false,
+    //             },
+    //             itemStyle: {
+    //               normal: {
+    //                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+    //                   {
+    //                     offset: 0,
+    //                     color: solarTheme.gradientLeft,
+    //                   },
+    //                   {
+    //                     offset: 1,
+    //                     color: solarTheme.gradientRight,
+    //                   },
+    //                 ]),
+    //                 shadowColor: solarTheme.shadowColor,
+    //                 shadowBlur: 0,
+    //                 shadowOffsetX: 0,
+    //                 shadowOffsetY: 3,
+    //               },
+    //             },
+    //             hoverAnimation: false,
+    //           },
+    //           {
+    //             value: 100 - this.value,
+    //             name: ' ',
+    //             tooltip: {
+    //               show: false,
+    //             },
+    //             label: {
+    //               normal: {
+    //                 position: 'inner',
+    //               },
+    //             },
+    //             itemStyle: {
+    //               normal: {
+    //                 color: config.variables.layoutBg,
+    //               },
+    //             },
+    //           },
+    //         ],
+    //       },
+    //       {
+    //         name: ' ',
+    //         clockWise: true,
+    //         hoverAnimation: false,
+    //         type: 'pie',
+    //         center: ['45%', '50%'],
+    //         radius: solarTheme.radius,
+    //         data: [
+    //           {
+    //             value: this.value,
+    //             name: ' ',
+    //             label: {
+    //               normal: {
+    //                 position: 'inner',
+    //                 show: false,
+    //               },
+    //             },
+    //             tooltip: {
+    //               show: false,
+    //             },
+    //             itemStyle: {
+    //               normal: {
+    //                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+    //                   {
+    //                     offset: 0,
+    //                     color: solarTheme.gradientLeft,
+    //                   },
+    //                   {
+    //                     offset: 1,
+    //                     color: solarTheme.gradientRight,
+    //                   },
+    //                 ]),
+    //                 shadowColor: solarTheme.shadowColor,
+    //                 shadowBlur: 7,
+    //               },
+    //             },
+    //             hoverAnimation: false,
+    //           },
+    //           {
+    //             value: 100,
+    //             name: ' ',
+    //             tooltip: {
+    //               show: false,
+    //             },
+    //             label: {
+    //               normal: {
+    //                 position: 'inner',
+    //               },
+    //             },
+    //             itemStyle: {
+    //               normal: {
+    //                 color: 'none',
+    //               },
+    //             },
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   });
+    // });
   }
 
   ngOnDestroy() {
-    this.themeSubscription.unsubscribe();
+    // this.themeSubscription.unsubscribe();
   }
 }
--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Http } from "@angular/http";
+import { ActivatedRoute } from "@angular/router";
+declare let window: any;
+declare let FB: any;
 
 @Component({
-  selector: 'ngx-categories',
-  styleUrls: ['./categories.component.scss'],
-  templateUrl: './categories.component.html',
+  selector: "ngx-categories",
+  styleUrls: ["./categories.component.scss"],
+  templateUrl: "./categories.component.html"
 })
 export class CategoriesComponent {
+  categories;
+  constructor(private http: Http, private route: ActivatedRoute) {
+    let that = this;
+    let botId = this.route.parent.snapshot.params.id;
+
+    FB.getLoginStatus(function(response) {
+      if (response.status === "connected") {
+        // the user is logged in and has authenticated your
+        // app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed
+        // request, and the time the access token
+        // and signed request each expire
+        let uid = response.authResponse.userID;
+        let accessToken = response.authResponse.accessToken;
+
+        that.http
+          .get(
+            "https://3klcm8k5x0.execute-api.eu-central-1.amazonaws.com/latest/bots/" +
+              botId +
+              "/categories?access_token=" +
+              accessToken
+          )
+          .map(response => response.json())
+          .subscribe(res => {
+            let _categories = res;
+
+            that.categories = _categories.sort((a, b) => {
+              return a.order - b.order;
+            });
+          });
+      } else if (response.status === "not_authorized") {
+        // the user is logged in to Facebook,
+        // but has not authenticated your app
+        window.location.replace("/#/auth");
+      } else {
+        // the user isn't logged in to Facebook.
+        window.location.replace("/#/auth");
+      }
+    });
+  }
 }
