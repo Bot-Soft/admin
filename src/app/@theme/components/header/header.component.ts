b46b3ecfb9c93ac6c49837dc09273b331d692673
Angel Tsvetkov <mrproper86@gmail.com>
2018-02-23 00:24:08 +0200

    update

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -4,6 +4,9 @@ import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
 
+declare let window: any;
+declare let FB: any;
+
 @Component({
   selector: 'ngx-header',
   styleUrls: ['./header.component.scss'],
@@ -14,7 +17,7 @@ export class HeaderComponent implements OnInit {
 
   @Input() position = 'normal';
 
-  user: any;
+  user: any = {};
 
   userMenu = [{ title: 'Log out' }];
 
@@ -25,8 +28,29 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.userService.getUsers()
-      .subscribe((users: any) => this.user = users.angel);
+    let that = this;
+    FB.getLoginStatus(function (response) {
+      if (response.status === "connected") {
+        // the user is logged in and has authenticated your
+        // app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed
+        // request, and the time the access token
+        // and signed request each expire
+        FB.api("/me", (response) => {
+          that.user.name = response.name;
+          that.user.picture = "http://graph.facebook.com/" + response.id + "/picture?type=square"
+        });
+
+      } else if (response.status === "not_authorized") {
+        // the user is logged in to Facebook,
+        // but has not authenticated your app
+        window.location.replace("/#/auth");
+
+      } else {
+        // the user isn't logged in to Facebook.
+        window.location.replace("/#/auth");
+      }
+    });
   }
 
   toggleSidebar(): boolean {
