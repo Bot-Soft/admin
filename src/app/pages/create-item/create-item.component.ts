7253ae669c7e22af9349f433647a8b05ef557b6b
Angel Tsvetkov <mrproper86@gmail.com>
2018-03-25 10:28:14 +0300

    Update

--- src/app/pages/create-item/create-item.component.ts
+++ src/app/pages/create-item/create-item.component.ts
@@ -157,8 +157,8 @@ export class CreateItemComponent {
 
   }
 
-  getButtonTypeDisplayValue(button){
-    switch(button.type){
+  getButtonTypeDisplayValue(button) {
+    switch (button.type) {
       case 'web_url': {
         return 'URL';
       }
@@ -175,16 +175,12 @@ export class CreateItemComponent {
   }
 
   isValidURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-      '((([a-z\d]([a-z\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locater
-    if (!pattern.test(str)) {
-      return false;
-    } else {
+    let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
       return true;
+    }
+    else {
+      return false;
     }
   }
 
@@ -253,7 +249,7 @@ export class CreateItemComponent {
         .map(response => response.json())
         .subscribe(res => {
           let navigateToUrl = "#/bot/" + this.botId + "/items";
-          if(this.category_id){
+          if (this.category_id) {
             navigateToUrl = "#/bot/" + this.botId + "/items?category_id=" + this.category_id;
           }
           window.location.replace(navigateToUrl);
